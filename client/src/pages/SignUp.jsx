be1bcd1a9436b1e9b5a2d638499d115a3406262c
zippostorm <toghkklli@gmail.com>
2024-10-04 11:51:44 +0300

    some fixes

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -25,6 +25,7 @@ const SignUp = () => {
       });
       const data = await res.json();
       if (data.success === false) {
+        setLoading(false);
         return setErrorMessages(data.message);
       }
       setLoading(false);
@@ -38,9 +39,9 @@ const SignUp = () => {
   };
   return (
     <div className="min-h-screen mt-20">
-      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-10">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row gap-10">
         {/* leftside */}
-        <div className="flex-1">
+        <div className="flex-1 md:mt-20">
           <Link to="/" className="font-bold dark:text-white text-4xl">
             <span className="px-2 py-1 mr-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
               zippostorm's
