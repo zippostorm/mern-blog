1dd0ec49f270a28921e9bda3c44033698097afec
zippostorm <toghkklli@gmail.com>
2024-12-07 15:37:46 +0200

    add create a post api route

---
+++ api/models/post.model.js
@@ -0,0 +1,38 @@
+import e from "express";
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      default:
+        "https://www.dawsoncollege.qc.ca/humanities/wp-content/uploads/sites/95/anatomy-of-a-blog-post-header-3.jpg",
+    },
+    category: {
+      type: String,
+      default: "uncategorized",
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+export default Post;
