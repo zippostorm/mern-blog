65017029b697a7c5128f5c8e1a5b94b3062c7868
zippostorm <toghkklli@gmail.com>
2024-12-08 19:57:36 +0200

    add image download on react quill form functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,22 +1,98 @@
 import { Alert, Button, FileInput, Select, TextInput } from "flowbite-react";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ReactQuill from "react-quill-new";
 import "react-quill/dist/quill.snow.css";
 import { bucketId, projectId } from "../appwrite";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import { useNavigate } from "react-router-dom";
+import { BiImageAdd } from "react-icons/bi";
 
 const CreatePost = () => {
   const navigate = useNavigate();
+  const filePickerRef = useRef();
+  const quillRef = useRef(null);
 
   const [file, setFile] = useState(null);
+  const [imageFileForma, setImageFileForma] = useState(null);
   const [imageFileUploading, setImageFileUploading] = useState(false);
   const [imageFileUploadError, setImageFileUploadError] = useState(null);
   const [imageFileUploadProgress, setImageFileUploadProgress] = useState(0);
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const [dataForm, setDataForm] = useState({});
   const [publishError, setPublishError] = useState(null);
+  const [imageFileFormaUploading, setImageFileFormaUploading] = useState(false);
+  const [imageFileFormaUploadError, setImageFileFormaUploadError] =
+    useState(null);
+  const [imageFileFormaUploadProgress, setImageFileFormaUploadProgress] =
+    useState(0);
+  const [imageFileFormaUrl, setImageFileFormaUrl] = useState(null);
+
+  useEffect(() => {
+    if (imageFileForma) {
+      console.log(imageFileForma);
+      uploadImageFileForma();
+    }
+  }, [imageFileForma]);
+
+  const uploadImageFileForma = async () => {
+    try {
+      const fileId = "unique()"; // Генерация уникального ID для файла
+
+      // Создаем FormData для загрузки файла
+      const formData = new FormData();
+      formData.append("fileId", fileId);
+      formData.append("file", imageFileForma);
+
+      // Создаем XMLHttpRequest для загрузки с отслеживанием прогресса
+      const xhr = new XMLHttpRequest();
+      xhr.open(
+        "POST",
+        `https://cloud.appwrite.io/v1/storage/buckets/${bucketId}/files`
+      );
+      xhr.setRequestHeader("x-appwrite-project", projectId); // Замените на ID вашего проекта
+
+      xhr.upload.onprogress = (event) => {
+        setImageFileFormaUploading(true);
+        setImageFileFormaUploadError(null);
+        if (event.lengthComputable) {
+          const percentage = Math.round((event.loaded / event.total) * 100);
+          setImageFileFormaUploadProgress(percentage);
+        }
+      };
+
+      xhr.onload = () => {
+        if (xhr.status === 201) {
+          const response = JSON.parse(xhr.responseText);
+          setImageFileFormaUploadProgress(null);
+          const imageId = response.$id;
+          const fileUrl = `https://cloud.appwrite.io/v1/storage/buckets/${bucketId}/files/${imageId}/view?project=${projectId}`;
+          setImageFileFormaUrl(fileUrl);
+          setImageFileFormaUploading(false);
+          const quill = quillRef.current.getEditor();
+          const range = quill.getSelection();
+          quill.insertEmbed(range ? range.index : 0, "image", fileUrl);
+        } else if (xhr.status === 400) {
+          setImageFileFormaUploadError(
+            "Upload failed: image have more than 10mb"
+          );
+          setImageFileFormaUploadProgress(null);
+          setImageFileFormaUrl(null);
+          setImageFileFormaUploading(false);
+        }
+      };
+
+      xhr.onerror = () => {
+        console.error("Upload error:", xhr.statusText);
+        setImageFileFormaUploading(false);
+      };
+
+      xhr.send(formData);
+    } catch (error) {
+      console.error("Error uploading file:", error);
+      setImageFileFormaUploading(false);
+    }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -104,9 +180,8 @@ const CreatePost = () => {
       setImageFileUploading(false);
     }
   };
-
   return (
-    <div className="p-3 max-w-3xl mx-auto">
+    <div className="p-3 max-w-4xl mx-auto">
       <h1 className="text-center text-3xl my-7 font-semibold">Create a post</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <div className="flex flex-col gap-4 sm:flex-row justify-between">
@@ -158,9 +233,12 @@ const CreatePost = () => {
             )}
           </Button>
         </div>
-        {imageFileUploadError && (
-          <Alert color="failure">{imageFileUploadError}</Alert>
-        )}
+        {imageFileUploadError ||
+          (imageFileFormaUploadError && (
+            <Alert color="failure">
+              {imageFileUploadError ?? imageFileFormaUploadError}
+            </Alert>
+          ))}
         {imageFileUrl && (
           <img
             src={imageFileUrl}
@@ -168,16 +246,43 @@ const CreatePost = () => {
             className="w-full h-72 object-cover"
           />
         )}
-        <ReactQuill
-          theme="snow"
-          placeholder="Write somethink..."
-          className="h-72 mb-12"
-          required
-          onChange={(value) => {
-            setDataForm({ ...dataForm, content: value });
-          }}
-        />
-        <Button type="submit" gradientDuoTone="purpleToPink">
+        <div className="flex flex-col">
+          <input
+            type="file"
+            accept="image/*"
+            onChange={(e) => setImageFileForma(e.target.files[0])}
+            ref={filePickerRef}
+            hidden
+          />
+          {imageFileFormaUploadProgress ? (
+            <div className="w-12 h-12 mb-3 ml-auto">
+              <CircularProgressbar
+                value={imageFileFormaUploadProgress}
+                text={`${imageFileFormaUploadProgress || 0}%`}
+              />
+            </div>
+          ) : (
+            <BiImageAdd
+              className="mb-3 text-3xl ml-auto"
+              onClick={() => filePickerRef.current.click()}
+            />
+          )}
+          <ReactQuill
+            ref={quillRef}
+            theme="snow"
+            placeholder="Write somethink..."
+            className="h-72 w-full mb-12"
+            required
+            onChange={(value) => {
+              setDataForm({ ...dataForm, content: value });
+            }}
+          />
+        </div>
+        <Button
+          type="submit"
+          gradientDuoTone="purpleToPink"
+          disabled={imageFileFormaUploadProgress || imageFileUploadProgress}
+        >
           Publish
         </Button>
         {publishError && (
