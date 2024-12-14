e8a5f54269952b4b4cab30223062db8a4b61a082
zippostorm <toghkklli@gmail.com>
2024-12-14 19:17:38 +0200

    test deploy

--- api/index.js
+++ api/index.js
@@ -1,52 +1,35 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotevn from "dotenv";
-import userRoutes from "./routes/user.route.js";
-import authRoutes from "./routes/auth.route.js";
-import postRoutes from "./routes/post.route.js";
-import commentRoutes from "./routes/comment.route.js";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import path from "path";
 
-dotevn.config();
+dotenv.config();
 
 mongoose
   .connect(process.env.MONGO)
-  .then(() => {
-    console.log("MongoDB is connected");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-const __dirname = path.resolve();
+  .then(() => console.log("MongoDB is connected"))
+  .catch((err) => console.log(err));
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000!");
-});
+// Объединяем все маршруты
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
 
-app.use("/api/user", userRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/post", postRoutes);
-app.use("/api/comment", commentRoutes);
+app.use("/api", [userRoutes, authRoutes, postRoutes, commentRoutes]);
 
+// Обслуживаем клиентскую сборку
+const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "/client/dist")));
-
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error";
-  res.status(statusCode).json({
-    success: false,
-    statusCode,
-    message,
-  });
-});
+// Запуск сервера
+app.listen(3000, () => console.log("Server running on port 3000!"));
