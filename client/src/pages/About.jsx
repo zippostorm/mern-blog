0ba6081bc6781b32f958b5bb73b10cad67042179
zippostorm <toghkklli@gmail.com>
2024-12-14 17:33:29 +0200

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="h-full m-auto">
+      <div className="max-w-2xl p-3 text-center">
+        <div>
+          <h1 className="font-semibold text-4xl text-center my-7">
+            About Zippostorm's Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Welcome to Zippostorm's Blog, a community where ideas come to
+              life. This platform is dedicated to fostering learning and growth
+              through shared experiences. Dive into a world of insights and
+              discover something valuable today.
+            </p>
+            <p>
+              This blog is designed to inspire curiosity and spark creativity.
+              Here, you can explore a variety of topics, learn from diverse
+              perspectives, and contribute your own knowledge. Together, we can
+              build a space for growth and connection.
+            </p>
+            <p>
+              At Zippostorm's Blog, every story matters. It's a hub for
+              exchanging ideas, gaining new insights, and supporting one another
+              on the journey of learning. I hope you leave with inspiration and
+              useful takeaways.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import CallToAction from "../components/CallToAction";
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <div className="min-h-full max-w-2xl m-auto flex flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold text-center">Projects</h1>
+      <p className="text-md text-center text-gray-500">
+        Check fun and useful projects using React JS / Next JS / with MERN
+        technologies and Tailwind CSS here
+      </p>
+      <CallToAction />
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
